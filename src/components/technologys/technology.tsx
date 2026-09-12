2f924f312123ff136cdb0c8d0de02a7363a81f00
Tanvir Ahamed <tanvirahamed01916@gmail.com>
2026-09-13 00:35:24 +0600

    icon added

--- src/components/technologys/technology.tsx
+++ src/components/technologys/technology.tsx
@@ -2,16 +2,30 @@ import { useEffect, useState } from 'react'
 function Technology() {
     const [technologies, setTechnologies] = useState<any[]>([])
 
-  useEffect(() => {
+    useEffect(() => {
+        console.log('Technology component loaded')
+    }, [])
 
-    console.log('Technology component loaded')
+    useEffect(() => {
+        fetch('/public/data.json')
+            .then((res) => res.json())
+            .then((data) => {
+                setTechnologies(data)
+            })
+    }, [])
 
-  }, [])
-  return (
-    <div>
-      Technology
-    </div>
-  )
+    console.log(technologies)
+
+    return (
+        <div>
+            {technologies.map((technology) => (
+                <div key={technology.id}>
+                    <img src={technology.icon} alt={technology.name} />
+                    <h2>{technology.name}</h2>
+                </div>
+            ))}
+        </div>
+    )
 }
 
 export default Technology
